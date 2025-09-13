67ba1edb3a4aa5638d141c9ce86cf0e7159fa93a
barnaste <stefanbarna10@gmail.com>
2025-09-13 19:36:30 -0400

    blog dynamic routing

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let data;
+    const { title, date, Content } = data;
+</script>
+
+{#if data.status == 404}
+    <h1>404 - Not Found</h1>
+    <p>The blog post you are looking for does not exist.</p>
+    <p><a href="/blog">Go back to the blog index</a></p>
+
+{:else if data.status == 500}
+    <h1>500 - Server Error</h1>
+    <p>There was an error loading the blog post.</p>
+    <p><a href="/blog">Go back to the blog index</a></p>
+    
+{:else}
+    <article>
+        <h1>{title}</h1>
+        <time>Published {date}</time>
+
+        <Content />
+    </article>
+{/if}
---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,18 @@
+export async function load({ params }) {
+    try {
+        const post = await import(`$lib/blogposts/${params.slug}.md`);
+        const { title, date } = post.metadata;
+        const Content = post.default;
+
+        return {
+            Content,
+            title,
+            date
+        };
+    } catch (error) {
+        return {
+            status: 404,
+            error: new Error("Post not found")
+        };
+    }
+}
