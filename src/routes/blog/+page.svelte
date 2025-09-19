30bac82ea203ede5b0127702eae9487aadbfca14
barnaste <stefanbarna10@gmail.com>
2025-09-19 18:42:49 -0400

    blog index hover effect

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,8 +4,8 @@
 
 <h1 class="text-2xl font-semibold mb-10">My Writing</h1>
 {#each data.posts as post}
-    <div class="mb-6">
-        <a href="/blog/{post.path}" class="text-lg hover:underline">{post.meta.title}</a>
+    <div class="mb-6 hover:translate-x-1 transition-transform">
+        <a href="/blog/{post.path}" class="text-lg hover:text-olive-dark transition-colors duration-200">{post.meta.title}</a>
         <p class="text-slate-500 dark:text-slate-400">{new Date(post.meta.date).toDateString()}</p>
     </div>
 {/each}
