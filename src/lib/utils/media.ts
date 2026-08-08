37df52ba210a95c62e2391c8a3b30f52a6cf59ce
barnaste <stefanbarna10@gmail.com>
2026-08-08 15:50:42 -0400

    media query store cleanup

--- src/lib/utils/media.ts
+++ src/lib/utils/media.ts
@@ -1,20 +1,17 @@
 import { readable } from "svelte/store";
-import { onMount } from "svelte";
+import { browser } from "$app/environment";
 
 export function mediaQuery(query: string) {
     return readable(false, (set) => {
-        let mql: MediaQueryList;
+        if (!browser) return;
 
-        // run on client only after mounting
-        onMount(() => {
-            mql = window.matchMedia(query);
-            const update = () => set(mql.matches);
+        const mql = window.matchMedia(query);
+        const update = () => set(mql.matches);
 
-            update(); // initial check
-            mql.addEventListener("change", update);
+        update(); // initial check
+        mql.addEventListener("change", update);
 
-            return () => mql.removeEventListener("change", update);
-        });
+        return () => mql.removeEventListener("change", update);
     });
 }
 
